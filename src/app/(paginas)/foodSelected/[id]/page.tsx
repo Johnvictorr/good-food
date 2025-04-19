5a7cf54fa1751b68f2d6b74b3620798bb43971ff
Johnvictorr <johnvictorxavierr@gmail.com>
2025-04-19 18:13:47 -0300

    [adjust - ui - complete] (Retoques na estilização de Telas) Atualizando estilização das telas

--- src/app/(paginas)/foodSelected/[id]/page.tsx
+++ src/app/(paginas)/foodSelected/[id]/page.tsx
@@ -77,17 +77,17 @@ export default function FoodSelected() {
     };
 
     return (
-        <div className="w-full grid grid-cols-1 md:grid-cols-3 text-black p-4 gap-5">
+        <div className="justify-center items-center flex flex-col md:flex-row w-full text-black p-4 gap-5">
 
-            <div className="w-full p-2 justify-center flex flex-col">
+            <div className="w-full md:w-1/3 justify-center flex flex-col">
 
-                <h1 className="flex items-center bg-red-600 w-auto mb-4 p-2 justify-center rounded-t-full text-lg md:text-2xl font-bold transition-transform duration-300 ease-in-out hover:-translate-y-2 text-shadow-lg text-white">Select quantity</h1>
+                <h1 className="flex items-center bg-red-600 w-auto mb-4 p-2 justify-center rounded-t-full text-lg md:text-2xl font-bold transition-transform duration-300 ease-in-out hover:-translate-y-2 text-shadow-base text-white">Select quantity</h1>
 
-                <div className="w-full h-60 md:h-88">
+                <div className="h-60 md:h-88">
                     <img className="w-full h-full rounded-lg" src="https://img.freepik.com/fotos-gratis/um-delicioso-hamburguer-no-estudio_23-2151846493.jpg?ga=GA1.1.663066825.1744765198&semt=ais_hybrid&w=740" alt="" />
                 </div>
 
-                <div className="flex justify-center pt-2 gap-1">
+                <div className="flex justify-center pt-3 gap-1">
                     <button className="w-10 rounded-lg bg-red-600 text-white font-bold border-red-500 cursor-pointer transition-transform duration-300 ease-in-out hover:-translate-y-1 text-xl"
                     onClick={decrementQtd}>-</button>
 
@@ -104,7 +104,7 @@ export default function FoodSelected() {
                         setQtd(1);
                         }
                     }}
-                    className="w-10 h-10 rounded-lg bg-gray-200 text-center"
+                    className="w-10 h-10 rounded-lg bg-gray-300 text-center"
                     />
 
                     <button className="w-10 rounded-lg bg-green-600 text-white font-bold border-green-500 cursor-pointer transition-transform duration-300 ease-in-out hover:-translate-y-1 text-xl"
@@ -114,75 +114,30 @@ export default function FoodSelected() {
 
             </div>
             
-            <div className="w-full p-2 justify-center flex flex-col">
+            <div className="w-full md:w-1/3 justify-center flex flex-col">
+                <h1 className="flex items-center bg-red-600 w-auto mb-4 p-2 justify-center rounded-t-full text-lg md:text-2xl font-bold transition-transform duration-300 ease-in-out hover:-translate-y-2 text-shadow-base text-white">Informations</h1>
 
-                <h1 className="flex items-center bg-red-600 w-auto mb-4 p-2 justify-center rounded-t-full text-md md:text-2xl font-bold transition-transform duration-300 ease-in-out hover:-translate-y-2 text-shadow-lg text-white">Client Information</h1>
-                <div className="flex flex-col">
-
-                    <label htmlFor="name">Client Name</label>
-                    <input 
-                        type="text"
-                        className="bg-gray-200 w-full h-10 rounded-lg p-2 focus:outline-none"
-                    />
-
-                    <label htmlFor="phone">Client Phone</label>
-                    <input 
-                        type="text"
-                        value={phone}
-                        onChange={handlePhoneChange}
-                        minLength={14}
-                        maxLength={14}
-                        className="bg-gray-200 w-full h-10 rounded-lg p-2 focus:outline-none"
-                    />
-
-                    <label htmlFor="address">Client Address</label>
-                    <input 
-                        type="text"
-                        placeholder="Street - Number"
-                        className="bg-gray-200 w-full h-10 rounded-lg p-2 focus:outline-none"
-                    />
-
-                    <label htmlFor="complement">Complement</label>
-                    <input 
-                        type="text"
-                        className="bg-gray-200 w-full h-10 rounded-lg p-2 mb-2 focus:outline-none"
-                    />
-
-                    <textarea 
-                        placeholder="Observations"
-                        className="bg-gray-200 h-36 rounded-lg p-3 focus:outline-none overflow-hidden"
-                    />
-
-                </div>
-                
-            </div>
-            <div className="w-full p-2 justify-center flex flex-col">
-                <h1 className="flex items-center bg-red-600 w-auto mb-4 p-2 justify-center rounded-t-full text-lg md:text-2xl font-bold transition-transform duration-300 ease-in-out hover:-translate-y-2 text-shadow-lg text-white">Total Price</h1>
-
-                <div className="flex flex-col text-xs md:text-lg gap-2 bg-gray-200 p-4 rounded-lg mb-5">
+                <div className="flex flex-col text-xs md:text-sm bg-gray-300 p-5 rounded-lg mb-5">
                     <h1>Product Value: <span className="font-bold">{`R$ ${item?.price}`}</span></h1>
                     <h1>Delivery Value: <span className="font-bold">R$ 5,00</span></h1>
-                    <h1 className="mb-7">Additional: R$ <span className="font-bold">0,00</span></h1>
 
-                    <h1 className="font-bold mb-5">Total: R$ {`${total}`}</h1>
+                    <h1 className="font-bold my-5">Total: R$ {`${total.toFixed(2)}`}</h1>
                 </div>
 
+                <label htmlFor="observation">Description</label>
+                <textarea className="flex flex-col h-27 resize-none text-xs md:text-sm bg-gray-300 p-2 rounded-lg mb-5"/>
+                    
                 <div className="flex flex-col w-full gap-4">
                     <input 
                         onClick={handleAddToCart}
                         type="button" 
                         value="Add to cart" 
-                        className="bg-amber-400 text-white font-bold h-10 rounded-lg cursor-pointer transition-transform duration-300 ease-in-out hover:-translate-y-1"
+                        className="bg-amber-400 text-white h-10 rounded-lg cursor-pointer transition-transform duration-300 ease-in-out hover:-translate-y-1"
                     />
                     <input 
                         type="button" 
-                        value="Checkout" 
-                        className="bg-green-600 text-white font-bold h-10 rounded-lg cursor-pointer transition-transform duration-300 ease-in-out hover:-translate-y-1"
-                    />
-                    <input 
-                        type="button" 
-                        value="Cancel" 
-                        className="bg-red-600 text-white font-bold h-10 rounded-lg cursor-pointer transition-transform duration-300 ease-in-out hover:-translate-y-1"
+                        value="Back to Home" 
+                        className="bg-gray-200 text-black h-10 rounded-lg cursor-pointer transition-transform duration-300 ease-in-out hover:-translate-y-1"
                     />
                 </div>
             </div>
--- src/componentes/header/header.tsx
+++ src/componentes/header/header.tsx
@@ -2,8 +2,7 @@
 
 import Link from "next/link";
 import { FaSearch } from "react-icons/fa";
-import { PiHamburger } from "react-icons/pi";
-import { TiShoppingCart } from "react-icons/ti";
+import { PiHamburger, PiShoppingCartSimple } from "react-icons/pi";
 
 import { useModalCartStore } from "@/store/modalCartStore";
 
@@ -11,17 +10,26 @@ export default function Header(){
     const { openModal } = useModalCartStore();
 
     return(
-        <header className="flex items-center justify-between py-4 px-3 md:px-8 border-b-1 border-gray-200">
+        <header className="flex items-center justify-between py-4 px-2 md:px-5 border-b-1 border-gray-300">
             <Link href={"/"}>
                 <div className="flex items-center gap-3 bg-red-600 md:pr-5 rounded-full cursor-pointer">
-                    <PiHamburger className="w-15 h-15 bg-red-600 text-amber-400 p-1 rounded-full" size={10}/>
+                    <PiHamburger className="w-10 h-10 md:w-15 md:h-15 bg-red-600 text-amber-400 p-1 rounded-full" size={10}/>
                     <span className="hidden md:block text-sm md:text-3xl text-white font-bold text-shadow-lg">G<span className="text-amber-400">OO</span>D-F<span className="text-amber-400">OO</span>D</span>
                 </div>
             </Link>
 
-            <button onClick={openModal}>
-                <TiShoppingCart
-                className="w-15 h-15 bg-red-600 text-amber-400 p-1 rounded-full"
+            <div className="flex overflow-hidden w-1/2 md:w-2/4 rounded-full">
+                <input 
+                    type="text"
+                    placeholder="Search"
+                    className="w-full rounded-l-full px-3 md:px-4 py-2 md:py-3 bg-gray-200 text-black focus:outline-none"
+                />
+                <button className="rounded-r-full px-3 md:px-4 cursor-pointer bg-gray-200 text-red-600 hover:text-white transition duration-100 ease-in-out"><FaSearch size={15} /></button>
+            </div>
+
+            <button className="rounded-full bg-red-600 hover:shadow-lg cursor-pointer shadow-gray-400 transition duration-300 ease-in-out" onClick={openModal}>
+                <PiShoppingCartSimple
+                className="w-10 h-10 md:w-15 md:h-15 text-amber-400 p-1 md:p-2"
                 size={10}
                 />
             </button>
